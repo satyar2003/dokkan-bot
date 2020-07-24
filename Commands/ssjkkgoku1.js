4db78be5b8f483d755d2ba9e07c5033001337455
satyar2003 <60124623+satyar2003@users.noreply.github.com>
2020-07-24 16:59:03 -0700

    Update ssjkkgoku1.js

--- Commands/ssjkkgoku1.js
+++ Commands/ssjkkgoku1.js
@@ -1,9 +1,20 @@
 module.exports = {
     name: 'ssjkkgoku1',
     description: 'PHY UR Super Saiyan Goku (Angel) (Super Kaioken)',
-    categories: ['', ''],
-    link: ['', ''],
-    status: 'incomplete',
+    categories: ['[Pure Saiyans](https://dbz-dokkanbattle.fandom.com/wiki/Pure_Saiyans)',
+                 '[Full Power](https://dbz-dokkanbattle.fandom.com/wiki/Full_Power)',
+                 '[Goku\'s Family](https://dbz-dokkanbattle.fandom.com/wiki/Goku%27s_Family)',
+                 '[Super Saiyans](https://dbz-dokkanbattle.fandom.com/wiki/Super_Saiyans)',
+                 '[Kamehameha](https://dbz-dokkanbattle.fandom.com/wiki/Kamehameha_(Category))',
+                 '[Otherworld Warriors](https://dbz-dokkanbattle.fandom.com/wiki/Otherworld_Warriors)'],
+    link: ['[Prepared for Battle](https://dbz-dokkanbattle.fandom.com/wiki/Prepared_for_Battle) (Ki +2)',
+           '[Shocking Speed](https://dbz-dokkanbattle.fandom.com/wiki/Shocking_Speed) (Ki +2)',
+           '[Super Saiyan](https://dbz-dokkanbattle.fandom.com/wiki/Super_Saiyan) (ATK +10%)',
+           '[Kamehameha](https://dbz-dokkanbattle.fandom.com/wiki/Kamehameha) (ATK +2500 when Super Attack is launched)',
+           '[Over in a Flash](https://dbz-dokkanbattle.fandom.com/wiki/Over_in_a_Flash) (Ki +3)',
+           '[Otherworld Warriors](https://dbz-dokkanbattle.fandom.com/wiki/Otherworld_Warriors_(Link_Skill)) (ATK +20%)',
+           '[Fierce Battle](https://dbz-dokkanbattle.fandom.com/wiki/Fierce_Battle) (ATK +15%)'],
+    status: 'complete',
     plural: 'false',
     aliases: ['sskkgoku1', 'Super Saiyan Goku (Angel) (Super Kaioken)'],
     execute(message, args) {
@@ -13,15 +24,15 @@ module.exports = {
         const {plural} = require('./ssjkkgoku1.js')
         const {aliases} = require('./ssjkkgoku1.js')
         var color = 7164715;
-        var name = "";
-        var url = "";
-        var desc = "";
-        var circle = "";
-        var character = "";
-        var leader = "";
-        var superatk = "";
-        var passive = "";
-        var stats = "HP: (55%)/ (100%)\nATK: (55%)/ (100%)\nDEF: (55%)/ (100%)";
+        var name = "Burning to the Last\nSuper Saiyan Goku (Angel) (Super Kaioken) [GLB]";
+        var url = "https://dbz-dokkanbattle.fandom.com/wiki/Burning_to_the_Last_Super_Saiyan_Goku_(Angel)_(Super_Kaioken)#Global";
+        var desc = "Super PHY UR";
+        var circle = "https://media.discordapp.net/attachments/712036120191434793/736369570012594276/card_1010660_circle.png";
+        var character = "https://media.discordapp.net/attachments/712036120191434793/736369591206281246/340.png";
+        var leader = "<:phy:729467520272039988> Type Ki +2, HP, ATK and DEF +50%";
+        var superatk = "[Super Kaioken Kamehameha](https://dbz-dokkanbattle.fandom.com/wiki/File:(Super_Attack)_Super_Saiyan_Goku_(Angel)_(Super_Kaioken)_-_Burning_to_the_Last): Causes immense damage to enemy while sacrificing 7% HP";
+        var passive = "Thundering Aura: Ki +3 and ATK +100% when HP is 50% or above";
+        var stats = "HP: 10,963 (55%)/14,363 (100%)\nATK: 10,371 (55%)/13,371 (100%)\nDEF: 6,590 (55%)/9,190 (100%)";
         var links = "";
             for (var i = 0; i < link.length; i++)
             {
@@ -32,10 +43,10 @@ module.exports = {
             {
                 cats += categories[i] + "\n";
             }
-        var apt = "APT: (unsupported)/ (supported)\nDefense: (unsupported)/ (supported) \nLinking Partner: [] \nTeam: [] \nBuild: Additional/ Critical";
-        var blp = "[] - links shared\n[] - links shared\n[] - links shared";
-        var deets = "-";
-        var footnotes = "[1]: ";
+        var apt = "APT: 1,921,390 (unsupported)/2,157,549 (supported)\nDefense: 33,084 (unsupported)/36,943 (supported) \nLinking Partner: [AGL UR Super Saiyan God SS Goku (https://dbz-dokkanbattle.fandom.com/wiki/Next-Level_Strike_Super_Saiyan_God_SS_Goku)] \nTeam: [Siblings' Bond](https://dbz-dokkanbattle.fandom.com/wiki/Siblings%27_Bond)/[Goku's Family](https://dbz-dokkanbattle.fandom.com/wiki/Goku%27s_Family) \nBuild: 11 Additional/15 Critical";
+        var blp = "[AGL UR Super Saiyan God SS Goku](https://dbz-dokkanbattle.fandom.com/wiki/Next-Level_Strike_Super_Saiyan_God_SS_Goku) - 6 links shared\n[AGL UR Super Saiyan God Goku](https://dbz-dokkanbattle.fandom.com/wiki/Flaring_Battle_Impulse_Super_Saiyan_God_Goku) - 5 links shared\n[TEQ UR Super Saiyan 4 Gogeta](https://dbz-dokkanbattle.fandom.com/wiki/Peerless_Gleam_Super_Saiyan_4_Gogeta) - 5 links shared";
+        var deets = "► 12 Ki Multiplier is 140%";
+        var footnotes = "";
         if (status === 'incomplete')
         {
             var person = aliases[aliases.length - 1]
