a77cd9da4843bd37ddd18d880e44909312ee472d
satyar2003 <60124623+satyar2003@users.noreply.github.com>
2020-07-25 22:58:49 -0700

    Update test.js

--- Commands/test.js
+++ Commands/test.js
@@ -1,13 +1,6 @@
 module.exports = {
     name: "test",
     execute(message, args){
-        var guild = client.guilds.cache.get(guildidhere);
-
-            if (!guild) return message.reply("The bot isn't in the guild with this ID.");
-
-
-        guild.fetchInvites()
-            .then(invites => message.channel.send('Found Invites:\n' + invites.map(invite => invite.code).join('\n')))
-            .catch(console.error);
+        message.reply("ur mom gay")
     }
 }
