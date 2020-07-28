92a52d93eaa0fdbac46a85600fd18c47ed6e4ee3
satyar2003 <60124623+satyar2003@users.noreply.github.com>
2020-07-28 16:57:38 -0700

    Update test.js

--- Commands/test.js
+++ Commands/test.js
@@ -3,26 +3,7 @@ const Discord = require("discord.js")
 module.exports = {
     name: "test",
     execute(message, args){
-        var x = args[0]
-        if (x === undefined)
-        {
-            message.reply("yee")
-        }
-        console.log(x)
-
-        /*switch (x)
-        {
-            case "gay":
-                message.reply("no u")
-                break;
-            
-            case "not":
-                message.reply("gay")
-                break;
-
-            default:
-                message.reply("hi")
-                break;
-        }*/
+        const Colors = {RED: 1, BLUE: 2, GREEN: 3};
+        message.channel.send(Colors.RED);
     }
 }
