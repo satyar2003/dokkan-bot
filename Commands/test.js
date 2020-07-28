2ab46faf445392899ec7bac2aa4721034fd0ab05
satyar2003 <60124623+satyar2003@users.noreply.github.com>
2020-07-27 19:17:11 -0700

    Update test.js

--- Commands/test.js
+++ Commands/test.js
@@ -1,6 +1,19 @@
+const Discord = require("discord.js")
+
 module.exports = {
     name: "test",
     execute(message, args){
-        message.reply("ur mom gay")
+        message.reply("hi")
+
+        switch (args[0].toLowerCase())
+        {
+            case "gay":
+                message.reply("no u")
+                break;
+            
+            case "not":
+                message.reply("gay")
+                break;
+        }
     }
 }
