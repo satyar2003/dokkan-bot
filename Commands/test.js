4bb3ec876bdf788fd3debd4cea13cee5742bc577
satyar2003 <60124623+satyar2003@users.noreply.github.com>
2020-07-28 17:00:12 -0700

    Update test.js

--- Commands/test.js
+++ Commands/test.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 module.exports = {
     name: "test",
     execute(message, args){
-        const Colors = {STR: "<:str:729468929293942894>", AGL: "<:agl:729467484289105930>", TEQ: "<:teq:729467504593862728>"};
+        const Colors = {STR: <:str:729468929293942894>, AGL: "<:agl:729467484289105930>", TEQ: "<:teq:729467504593862728>"};
         message.channel.send(Colors.RED);
     }
 }
