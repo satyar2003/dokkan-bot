2003b077a35f56fc4c86bfd0fef077144a82c361
satyar2003 <60124623+satyar2003@users.noreply.github.com>
2020-08-09 13:48:30 -0700

    google-sheets-done

--- Commands/test.js
+++ Commands/test.js
@@ -3,17 +3,6 @@ const Discord = require('discord.js')
 module.exports = {
     name: "test",
     execute(message, args){
-        const reader = require('../g-sheets-api')
-        const readerOptions = {
-        sheetId: "167S0pcL_MObdrHi2MWgf_CW1geR6dWOWISb3iKH9kU8",
-        returnAllResults: false,
-        filter: {
-            "key to filter on": "value to match",
-        },
-        };
-
-        reader(readerOptions, (results) => {
-        /* Do something amazing with the results */
-        });
+        
     }
 }
